f77d7d5e926cf788c4be831f1401a2d6bbca2981
XpecialOne <sofiane.masaif@gmail.com>
2020-11-12 01:35:20 +0100

    YouTube trailers integration

--- src/Components/Row.js
+++ src/Components/Row.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect} from 'react';
 import axios from '../axios';
 import "../Components/row.css";
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
+
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
 
 function Row( {title, fetchURL, isLargeRow } ) { 
     const [movies, setMovies] = useState( [] );
+    const [trailerUrl, setTrailerUrl] = useState("");
+
 
 
     // A snippet of code which runs based on a specific condition
@@ -23,6 +28,28 @@ function Row( {title, fetchURL, isLargeRow } ) {
 
     }, [fetchURL]);
 
+    // YouTube canvas
+    const opts = {
+        height: "450",
+        width: "100%",
+        playerVars: {
+          autoplay: 0,
+        }
+      }
+
+      const handleClick = (movie) => {
+        // console.table(movie?.title)
+        if (trailerUrl) {
+          setTrailerUrl('')
+        } else {
+          movieTrailer(movie?.title || "")
+            .then(url => {
+              const urlParams = new URLSearchParams(new URL(url).search);
+              setTrailerUrl(urlParams.get('v'));
+            }).catch((error) => console.log(error));
+        }
+      }     
+
 
     return (
         <div className="row">
@@ -34,11 +61,18 @@ function Row( {title, fetchURL, isLargeRow } ) {
                  {movies.map(movie => (
                      <img 
                      key={movie.id}  // optimisation tip for a smoother scrolling of the row images
+                     onClick={() => handleClick(movie)}
                      className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+                     src={`${base_url}${
+                         isLargeRow ? movie.poster_path : movie.backdrop_path
+                         }`} 
+                         alt={movie.name} />
                  ))}
             </div> 
-           
+        {/* Youtube trailers div */}
+          <div className="yt">
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div> 
             
         </div>
     )
